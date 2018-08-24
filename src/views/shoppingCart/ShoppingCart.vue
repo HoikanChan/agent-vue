9d313978940d95ff5338b2bf0246a4458ba19fca
chenhq <chenhq@rsclouds.com>
2018-08-24 17:30:05 +0800

    购物车init

--- src/views/shoppingCart/ShoppingCart.vue
+++ src/views/shoppingCart/ShoppingCart.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="shoppingcart">
-    <div class="shoppingcart_top">购物车</div>
+    <x-header :left-options="{backText: ''}">购物车</x-header>
     <div class="editer">
       <img style="" src="../../assets/images/19.jpg" />
       <p>不问明天</p>
       <span v-if="flag" @click="editer">{{span}}</span>
     </div>
-    <ul class="order_list">
+    <checker v-model="picked" default-item-class="demo2-item" selected-item-class="address-selected" radio-required>
+      <checker-item v-for="(item,index) in products" :key="index" :value="item.id">
+        <div class="address-detail">
+          <div class="checkbox-wrapper">
+            <label class="material-checkbox">
+              <input type="checkbox">
+              <span></span>
+            </label>
+          </div>
+          <div>
+            <p>好吃的可爱多
+              <span>(10支/一盒)</span>
+            </p>
+            <p>产品信息
+              <span>10支/一盒</span>
+            </p>
+            <p>乳液体</p>
+            <p>￥990.00</p>
+            <span class="number" v-if="nums">×12</span>
+            <el-input-number size="mini" v-model="num" :min="1" v-if="!nums"></el-input-number>
+          </div>
+        </div>
+
+      </checker-item>
+    </checker>
+    <!-- <ul class="order_list">
       <li>
         <div>
           <el-checkbox label=""></el-checkbox>
@@ -43,16 +68,41 @@
           <el-input-number size="mini" v-model="num" :min="1" v-if="!nums"></el-input-number>
         </div>
       </li>
-    </ul>
+    </ul> -->
     <v-shopcart></v-shopcart>
   </div>
 </template>
 <script>
 import ShopCart_Footer from 'components/public/ShopCart_Footer.vue'
-import { Checklist } from 'mint-ui'
+import { XHeader, Checker, CheckerItem } from 'vux'
 export default {
+  components: {
+    XHeader,
+    Checker,
+    CheckerItem,
+    'v-shopcart': ShopCart_Footer
+  },
   data() {
     return {
+      picked: '',
+      products: [
+        {
+          img: 'http://p0.meituan.net/600.600/deal/__38666717__4597520.jpg',
+          title: '丰胸精油盒装（10支/一盒）',
+          price: '900',
+          info: '（10支/一盒）',
+          category: '乳液体',
+          amount: '1'
+        },
+        {
+          img: 'http://p0.meituan.net/600.600/deal/__38666717__4597520.jpg',
+          title: '丰胸精油盒装（10支/一盒）',
+          price: '900',
+          info: '（10支/一盒）',
+          category: '乳液体',
+          amount: '1'
+        }
+      ],
       msg: '购物车',
       checked: true,
       span: '编辑',
@@ -77,12 +127,12 @@ export default {
         this.span = '完成'
       }
     }
-  },
-  components: {
-    'v-shopcart': ShopCart_Footer
   }
 }
 </script>
+<style lang="less" scoped>
+</style>
+
 <style>
 @import url('../../assets/css/shoppingcart.css');
 .el-input-number--mini {
