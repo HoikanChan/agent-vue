2223734fb0aa185f37def8b9b4c5e97d5103c4b2
chenhq <chenhq@rsclouds.com>
2018-08-28 14:31:41 +0800

    我的订单解决冲突

--- src/views/myorder/MyOrder.vue
+++ src/views/myorder/MyOrder.vue
@@ -113,30 +113,8 @@ export default {
 }
 </script>
 <style lang="less">
-<<<<<<< HEAD
-.myorder{
-    .myorder_head{
-        height: .44rem;
-        line-height: .44rem;
-        padding: 0 4.8%;
-        text-align: center;
-        font-size: .18rem;
-        border-bottom: 1px solid #ccc;
-        .back{
-            float: left;
-            width: .1rem;
-            height: .16rem;
-            margin-top: .15rem;
-           
-        }
-    }
-    .things{
-        height: .41rem;
-        padding: 0 4.8%;
-=======
 .myorder {
   .myorder_head {
-    width: 100%;
     height: 0.44rem;
     line-height: 0.44rem;
     padding: 0 4.8%;
@@ -151,7 +129,6 @@ export default {
     }
   }
   .things {
-    width: 100%;
     height: 0.41rem;
     padding: 0 4.8%;
     background: #fff;
@@ -176,10 +153,8 @@ export default {
     margin-top: 0.1rem;
     .first {
       .shophead {
-        width: 100%;
         height: 0.4rem;
         line-height: 0.4rem;
->>>>>>> 3c70d07d0e2fd48f0345335e418da3a32f928219
         background: #fff;
         padding: 0 4.8%;
         img {
@@ -196,88 +171,34 @@ export default {
           font-size: 0.13rem;
           color: #5b50d3;
         }
-    }
-    .shoppings{
-        margin-top: .1rem;
-        .first{
-            .shophead{
-                height: .4rem;
-                line-height: .4rem;
-                background: #fff;
-                padding: 0 4.8%;
-                img{
-                    width: .23rem;
-                    height: .23rem;
-                    border-radius: 50%;
-                }
-                span{
-                    padding-left: 1.2%;
-                    font-size: .15rem;
-                }
-                i{
-                    float: right;
-                    font-size: .13rem;
-                    color: #5b50d3;
-                }
+      }
+      ul {
+        li {
+          height: 1.41rem;
+          padding: 0 4.8%;
+          .left {
+            width: 1.1rem;
+            height: 1.1rem;
+            margin-top: 0.15rem;
+            float: left;
+            img {
+              width: 1.1rem;
+              height: 1.1rem;
             }
-            ul{
-                li{
-                    height: 1.41rem;
-                    padding: 0 4.8%;
-                    .left{
-                        width: 1.1rem;
-                        height: 1.1rem;
-                        margin-top: .15rem;
-                        float: left;
-                        img{
-                            width: 1.1rem;
-                            height: 1.1rem;
-                        }
-                    }
-                    .right{
-                        float: right;
-                        width: 60%;
-                        height: 1.41rem;
-                        .name{
-                            font-size: .14rem;
-                            margin-top: .12rem;
-                            font-weight: bold;
-                        }
-                        .msg{
-                            font-size: .13rem;
-                            color: #666;
-                            margin-top: .06rem;
-                        }
-                        .msg_{
-                            font-size: .13rem;
-                            color: #666;
-                        }
-                        .price{
-                            font-size: .2rem;
-                            color: #5b50d3;
-                            font-weight: bold;
-                            margin-top: .28rem;
-                            display: inline-block;
-                        }
-                        .number{
-                            float: right;
-                            margin-top: .28rem;
-                            font-size: .2rem;
-                        }
-                    }
-                }
+          }
+          .right {
+            float: right;
+            width: 60%;
+            height: 1.41rem;
+            .name {
+              font-size: 0.14rem;
+              margin-top: 0.12rem;
+              font-weight: bold;
             }
-            .add{
-                height: .43rem;
-                line-height: .43rem;
-                border-bottom: 1px solid #ccc;
-                background: #fff;
-                font-size: .13rem;
-                text-align: right;
-                padding-right: 4.8%;
-                i{
-                    font-size: .15rem;
-                }
+            .msg {
+              font-size: 0.13rem;
+              color: #666;
+              margin-top: 0.06rem;
             }
             .msg_ {
               font-size: 0.13rem;
@@ -299,7 +220,6 @@ export default {
         }
       }
       .add {
-        width: 100%;
         height: 0.43rem;
         line-height: 0.43rem;
         border-bottom: 1px solid #ccc;
@@ -311,30 +231,58 @@ export default {
           font-size: 0.15rem;
         }
       }
-      .logistics {
-        width: 100%;
-        height: 0.53rem;
-        background: #fff;
-        a {
-          display: block;
-          width: 16.5%;
-          height: 0.22rem;
-          line-height: 0.22rem;
-          text-align: center;
-          border: 1px solid #999;
-          float: right;
-          border-radius: 0.03rem;
-          margin-right: 4.8%;
-          margin-top: 0.15rem;
-          font-size: 0.13rem;
-          color: #000;
-        }
-        .blue {
-          color: #5b50d3;
-          border: 1px solid #5b50d3;
-        }
+      .msg_ {
+        font-size: 0.13rem;
+        color: #666;
+      }
+      .price {
+        font-size: 0.2rem;
+        color: #5b50d3;
+        font-weight: bold;
+        margin-top: 0.28rem;
+        display: inline-block;
+      }
+      .number {
+        float: right;
+        margin-top: 0.28rem;
+        font-size: 0.2rem;
       }
     }
+  }
+}
+.add {
+  height: 0.43rem;
+  line-height: 0.43rem;
+  border-bottom: 1px solid #ccc;
+  background: #fff;
+  font-size: 0.13rem;
+  text-align: right;
+  padding-right: 4.8%;
+  i {
+    font-size: 0.15rem;
+  }
+}
+.logistics {
+  width: 100%;
+  height: 0.53rem;
+  background: #fff;
+  a {
+    display: block;
+    width: 16.5%;
+    height: 0.22rem;
+    line-height: 0.22rem;
+    text-align: center;
+    border: 1px solid #999;
+    float: right;
+    border-radius: 0.03rem;
+    margin-right: 4.8%;
+    margin-top: 0.15rem;
+    font-size: 0.13rem;
+    color: #000;
+  }
+  .blue {
+    color: #5b50d3;
+    border: 1px solid #5b50d3;
   }
 }
 
