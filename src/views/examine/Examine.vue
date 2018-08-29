d45e09f8f406118c7784ca0267d5f1c9b664b066
chenhq <chenhq@rsclouds.com>
2018-08-29 09:40:25 +0800

    审核init

--- src/views/examine/Examine.vue
+++ src/views/examine/Examine.vue
@@ -1,65 +1,81 @@
 <template>
-    <div class="examine">
-        <x-header :left-options="{backText: ''}">审核</x-header>
-        <div class="tab">
-            <div>
-                <img src="../../assets/images/register_check.png" /><img src="../../assets/images/register_check_.png" />
-                <p>注册审核</p>
-            </div>
-            <div>
-                <img src="../../assets/images/upgrade_check.png" /><img src="../../assets/images/upgrade_check_.png" />
-                <p>升级审核</p>
-            </div>
-        </div>
+  <div class="examine">
+    <x-header :left-options="{backText: ''}">审核</x-header>
+    <div class="tab">
+      <div @click="tabNow='register'">
+        <img src="../../assets/images/register_check.png" v-if="tabNow==='register'" />
+        <img src="../../assets/images/register_check_.png" v-else/>
+        <p>注册审核</p>
+        <badge text="8" class="badge"></badge>
+      </div>
+      <div @click="tabNow='upgrade'">
+        <img src="../../assets/images/upgrade_check.png" v-if="tabNow==='upgrade'" />
+        <img src="../../assets/images/upgrade_check_.png" v-else/>
+        <p>升级审核</p>
+        <badge text="8" class="badge"></badge>
+      </div>
     </div>
+    <div class="tab-content" v-show="tabNow==='register'">
+      aaa
+    </div>
+    <div class="tab-content" v-show="tabNow==='upgrade'">
+      bbb
+    </div>
+  </div>
 </template>
 <script>
-import { XHeader } from 'vux'
+import { XHeader, Badge } from 'vux'
 export default {
-    data(){
-        return{}
-
-    },
-    components:{
-        XHeader
+  data() {
+    return {
+      tabNow: 'register'
     }
+  },
+  components: {
+    XHeader,
+    Badge
+  }
 }
 </script>
-<style lang="less">
-    .examine{
-        .tab{
-            width: 100%;
-            height: 1.1rem;
-            border-top: 1px solid #ccc;
-            background: #fff;
-            >div{
-                width: 50%;
-                height: 1.1rem;
-                float: left;
-                text-align: center;
-                img{
-                    margin-top: .3rem;
-                }
-                &:first-child{
-                    img{
-                        width: .4rem;
-                        height: .37rem;
-                    }
-                }
-                &:nth-child(2){
-                    img{
-                        width: .39rem;
-                        height: .37rem;
-                    }
-                    
-                }
-                
-            }
+<style lang="less" scoped>
+.examine {
+  .tab {
+    width: 100%;
+    height: 1.1rem;
+    border-top: 1px solid #ccc;
+    background: #fff;
+    display: flex;
+    > div {
+      width: 50%;
+      position: relative;
+      height: 1.1rem;
+      text-align: center;
+      .badge {
+        position: absolute;
+        top: 0.23rem;
+        right: 0.66rem;
+      }
+      img {
+        margin-top: 0.3rem;
+      }
+      &:first-child {
+        img {
+          width: 0.4rem;
+          height: 0.37rem;
         }
+      }
+      &:nth-child(2) {
+        img {
+          width: 0.39rem;
+          height: 0.37rem;
+        }
+      }
     }
-    .show{
-        color: #5b50d3;
-    }
+  }
+}
+.show {
+  color: #5b50d3;
+}
 </style>
 
 
