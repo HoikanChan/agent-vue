c09a86e3f68a4cda6d71888a630281089611d840
hoikan <hoikan.chen@gmail.com>
2018-08-31 20:53:45 +0800

    my team

--- src/views/myteam/MyTeam.vue
+++ src/views/myteam/MyTeam.vue
@@ -23,246 +23,225 @@
             </div>
         </div>
         <div class="level">
-            <router-link to="#" @click="tabNow='partner'" v-for="(item,key) in member" :key="key">
+            <a to="#" @click="switchTab(key,item)" v-for="(item,key) in member" :key="key">
                 <p class="first" >{{item.count}}</p>
                
                 <p class="second">{{key}}</p>
-            </router-link>
-            <!-- <router-link to="#">
-                <p class="first" @click="tabNow='all'">10</p>
-                <p class="second">总代</p>
-            </router-link>
-            <router-link to="#">
-                <p class="first" @click="tabNow='big'">10</p>
-                <p class="second">大区</p>
-            </router-link>
-            <router-link to="#" class="none" @click="tabNow='vip'">
-                <p class="first">10</p>
-                <p class="second">VIP</p>
-            </router-link> -->
+            </a>
         </div>
         <div>
-            <ul class="members" v-show="tabNow==='partner'">
-                <li v-for="(item,key) in member[2]" :key="key">
-                    <img class="jpg" src="../../assets/images/15.jpg" />
-                    <span>{{key}}</span>
-                    <img class="right" src="../../assets/images/right.png" />
+            <ul class="members"  >
+                <li v-for="(item,key) in pickedTeam":key="key">
+                  <img class="jpg" src="../../assets/images/15.jpg" />
+                  <span>{{key}}</span>
+                  <img class="right" src="../../assets/images/right.png" />
                 </li>
             </ul>
-            <ul class="members" v-show="tabNow==='all'">
-                <li v-for="(item,key) in member[3]" :key="key">
-                    <img class="jpg" src="../../assets/images/15.jpg" />
-                    <span>{{key}}</span>
-                    <img class="right" src="../../assets/images/right.png" />
-                </li>
-            </ul>
-            <ul class="members" v-show="tabNow==='big'">
-                <li v-for="(item,key) in member[1]" :key="key">
-                    <img class="jpg" src="../../assets/images/15.jpg" />
-                    <span>{{key}}</span>
-                    <img class="right" src="../../assets/images/right.png" />
-                </li>
-            </ul>
-            <ul class="members" v-show="tabNow==='vip'">
-                <li v-for="(item,key) in member[0]" :key="key">
-                    <img class="jpg" src="../../assets/images/15.jpg" />
-                    <span>{{key}}</span>
-                    <img class="right" src="../../assets/images/right.png" />
-                </li>
-            </ul>
+            <div v-if="pickedTeam.length === 0"> no record</div>
         </div>
     </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from 'axios'
 export default {
-    data(){
-        return{
-            msg:'我的团队',
-            list:[],
-            tabNow:'partner',
-            referrer:'',
-            direct:'',
-            member:[]
-        }
-    },
-    mounted() {
-        axios.get('http://124.200.40.10:17080/agent/api/v1/team/referrer').then((response)=>{
-            this.referrer=response.data.data.username
-            axios.get('http://124.200.40.10:17080/agent/api/v1/team/parent').then((res)=>{
-                this.direct=res.data.data.username
-            })
-            axios.get('http://124.200.40.10:17080/agent/api/v1/team/children').then((result)=>{
-                // console.log(result.data.data)
-                this.member=result.data.data
-                console.log(this.member)
-                // console.log(JSON.stringify(this.member))
-            })
-        })
-
-    },
+  data() {
+    return {
+      msg: '我的团队',
+      list: [],
+      tabNow: 'partner',
+      pickedTeam: {},
+      referrer: '',
+      direct: '',
+      member: []
+    }
+  },
+  methods: {
+    switchTab(tabName, item) {
+      this.pickedTeam = item.users
+      this.tabNow = tabName
+    }
+  },
+  mounted() {
+    axios
+      .get('http://124.200.40.10:17080/agent/api/v1/team/referrer')
+      .then(response => {
+        this.referrer = response.data.data.username
+        axios
+          .get('http://124.200.40.10:17080/agent/api/v1/team/parent')
+          .then(res => {
+            this.direct = res.data.data.username
+          })
+        axios
+          .get('http://124.200.40.10:17080/agent/api/v1/team/children')
+          .then(result => {
+            // console.log(result.data.data)
+            this.member = result.data.data
+            console.log(this.member)
+            // console.log(JSON.stringify(this.member))
+          })
+      })
+  }
 }
 </script>
 <style lang="less">
-    .myteam{
-        .team_head{
-            height: .44rem;
-            text-align: center;
-            padding: 0 4.8%;
-            line-height: .44rem;
-            font-size: .16rem;
-            color: #000;
-            .back{
-                float: left;
-                width: .1rem;
-                height: .16rem;
-                margin-top: .15rem;
-            }
-            .cash{
-                float: right;
-                width: .2rem;
-                height: .2rem;
-                margin-top: .12rem;
-                margin-right: .01rem;
-            }
-        }
-        .agency{
-            width: 100%;
-            height: 1.42rem;
-            background: #5b50d3;
-            position: relative;
-            input{
-                display: inline-block;
-                width: 59.2%;
-                height: .28rem;
-                line-height: .28rem;
-                background: #fff;
-                margin-top: .14rem;
-                margin-left: 11.4%;
-                padding-left: 18%;
-                border-radius: .03rem;
-                font-family: '苹方';
-                font-size: .13rem;
-            }
-            .img{
-                width: .16rem;
-                height: .16rem;
-                position: absolute;
-                left: 24.1%;
-                top:.19rem;
-            }
-            .realtion{
-                width: 21.4%;
-                height: .77rem;
-                margin-left: 19.2%;
-                margin-top: .11rem;
-                text-align: center;
-                float: left;
-                .man{
-                    color: #fff;
-                    height: .24rem;
-                    line-height: .24rem;
-                }
-                .head{
-                    width: .3rem;
-                    height: .3rem;
-                    position: inherit;
-                    border-radius: 50%;
-                    border: 1px solid #fff;
-                }
-                .mantwo{
-                    color: #fff;
-                    height: .2rem;
-                    line-height: .2rem;
-                }
-            }
-            .relate{
-                float: left;
-                width: .46rem;
-                height: .15rem;
-                margin-top: .44rem;
-                margin-left: 3.3%;
-            }
-            .realtion_{
-                width: 21.4%;
-                height: .77rem;
-                margin-left: 3.3%;
-                margin-top: .11rem;
-                text-align: center;
-                float: left;
-                .man_{
-                    color: #fff;
-                    height: .24rem;
-                    line-height: .24rem;
-                }
-                .head_{
-                    width: .3rem;
-                    height: .3rem;
-                    position: inherit;
-                    border-radius: 50%;
-                    border: 1px solid #fff;
-                }
-                .mantwo_{
-                    color: #fff;
-                    height: .2rem;
-                    line-height: .2rem;
-                }
-            }
-        }
-        .level{
-            width: 100%;
-            height: .71rem;
-            background: #fff;
-            a{
-                display: block;
-                width: 24.7%;
-                height: .4rem;
-                float: left;
-                text-align: center;
-                border-right: 1px solid #ccc;
-                margin-top: .15rem;
-                font-size: .13rem;
-                .first{
-                    height: .17rem;
-                    line-height: .17rem;
-                }
-                .second{
-                    height: .22rem;
-                    line-height: .22rem;
-                    margin-top: 0;
-                }
-                &:last-child{
-                    border-right: none;
-                }
-            }
-            .none{
-                border-right: none;
-            }
-          
-        }
-        .members{
-            background: #fff;
-            margin-top: .1rem;
-            li{
-                height: .53rem;
-                line-height: .53rem;
-                border-bottom: 1px solid #ccc;
-                padding: 0 4.8%;
-                .jpg{
-                    width: .33rem;
-                    height: .33rem;
-                    border-radius: 50%;
-                }
-                span{
-                    margin-top: .2rem;
-                    margin-left: 2%;
-                }
-                .right{
-                    width: .09rem;
-                    height: .16rem;
-                    margin-top: .19rem;
-                }
-            }
-        }
+.myteam {
+  .team_head {
+    height: 0.44rem;
+    text-align: center;
+    padding: 0 4.8%;
+    line-height: 0.44rem;
+    font-size: 0.16rem;
+    color: #000;
+    .back {
+      float: left;
+      width: 0.1rem;
+      height: 0.16rem;
+      margin-top: 0.15rem;
     }
+    .cash {
+      float: right;
+      width: 0.2rem;
+      height: 0.2rem;
+      margin-top: 0.12rem;
+      margin-right: 0.01rem;
+    }
+  }
+  .agency {
+    width: 100%;
+    height: 1.42rem;
+    background: #5b50d3;
+    position: relative;
+    input {
+      display: inline-block;
+      width: 59.2%;
+      height: 0.28rem;
+      line-height: 0.28rem;
+      background: #fff;
+      margin-top: 0.14rem;
+      margin-left: 11.4%;
+      padding-left: 18%;
+      border-radius: 0.03rem;
+      font-family: '苹方';
+      font-size: 0.13rem;
+    }
+    .img {
+      width: 0.16rem;
+      height: 0.16rem;
+      position: absolute;
+      left: 24.1%;
+      top: 0.19rem;
+    }
+    .realtion {
+      width: 21.4%;
+      height: 0.77rem;
+      margin-left: 19.2%;
+      margin-top: 0.11rem;
+      text-align: center;
+      float: left;
+      .man {
+        color: #fff;
+        height: 0.24rem;
+        line-height: 0.24rem;
+      }
+      .head {
+        width: 0.3rem;
+        height: 0.3rem;
+        position: inherit;
+        border-radius: 50%;
+        border: 1px solid #fff;
+      }
+      .mantwo {
+        color: #fff;
+        height: 0.2rem;
+        line-height: 0.2rem;
+      }
+    }
+    .relate {
+      float: left;
+      width: 0.46rem;
+      height: 0.15rem;
+      margin-top: 0.44rem;
+      margin-left: 3.3%;
+    }
+    .realtion_ {
+      width: 21.4%;
+      height: 0.77rem;
+      margin-left: 3.3%;
+      margin-top: 0.11rem;
+      text-align: center;
+      float: left;
+      .man_ {
+        color: #fff;
+        height: 0.24rem;
+        line-height: 0.24rem;
+      }
+      .head_ {
+        width: 0.3rem;
+        height: 0.3rem;
+        position: inherit;
+        border-radius: 50%;
+        border: 1px solid #fff;
+      }
+      .mantwo_ {
+        color: #fff;
+        height: 0.2rem;
+        line-height: 0.2rem;
+      }
+    }
+  }
+  .level {
+    width: 100%;
+    height: 0.71rem;
+    background: #fff;
+    a {
+      display: block;
+      width: 24.7%;
+      height: 0.4rem;
+      float: left;
+      text-align: center;
+      border-right: 1px solid #ccc;
+      margin-top: 0.15rem;
+      font-size: 0.13rem;
+      .first {
+        height: 0.17rem;
+        line-height: 0.17rem;
+      }
+      .second {
+        height: 0.22rem;
+        line-height: 0.22rem;
+        margin-top: 0;
+      }
+      &:last-child {
+        border-right: none;
+      }
+    }
+    .none {
+      border-right: none;
+    }
+  }
+  .members {
+    background: #fff;
+    margin-top: 0.1rem;
+    li {
+      height: 0.53rem;
+      line-height: 0.53rem;
+      border-bottom: 1px solid #ccc;
+      padding: 0 4.8%;
+      .jpg {
+        width: 0.33rem;
+        height: 0.33rem;
+        border-radius: 50%;
+      }
+      span {
+        margin-top: 0.2rem;
+        margin-left: 2%;
+      }
+      .right {
+        width: 0.09rem;
+        height: 0.16rem;
+        margin-top: 0.19rem;
+      }
+    }
+  }
+}
 </style>
