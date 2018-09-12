6c065b7cda1860dad82cca0825e5ff13294e98bb
chenhq <chenhq@rsclouds.com>
2018-09-12 16:50:54 +0800

    fix bug

--- src/views/buy/Buy.vue
+++ src/views/buy/Buy.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="forget-psw-container">
-    <x-header :left-options="{backText: ''}" >
+    <x-header :left-options="{backText: ''}">
       <span>确认下单</span>
       <img slot="overwrite-left" src="../../assets/images/back.png" size="25" style="width:.09rem;height:auto;position:relative;top:-2px;" @click="$router.push({name:'mall'})">
     </x-header>
@@ -166,6 +166,7 @@ export default {
             text: result.errmsg
           })
         } else {
+          this.$store.dispatch('setBill', '')
           this.$router.push({ name: 'bought' })
         }
       }
